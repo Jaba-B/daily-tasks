f22c0df64c0b0276ce9d81b1cf29176c9bddf644
Jaba Beroshvili <jb.beroshvili@gmail.com>
2022-10-13 10:43:45 +0400

    best time to buy and sell stock

--- task.js
+++ task.js
@@ -1,21 +1,27 @@
-// Two Sum
+// Best Time to Buy and Sell Stock
 
-// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-// Example 1:   Input: nums = [2,7,11,15], target = 9;      Output: [0,1]
-// Example 2:   Input: nums = [3,2,4], target = 6;      Output: [1,2]   
-// Example 3:   Input: nums = [3,3], target = 6;      Output: [0,1]
-
-function twoSum(arr, target) {
-    for (i = 0; i < arr.length; i++) {    //O(n)
-      let searchNum = target - arr[i];
-      if(arr.indexOf(searchNum, i+1) > 0) {    //O(n)
-        return [i, arr.indexOf(searchNum, i+1)];
-      }
-    }
+function buyAndSellStocks(arr) {
+  let minPrice = Math.min(...arr)  // O(n)
+  let pricesAfterBuying = arr.slice(arr.indexOf(minPrice) + 1) // O(n)
+  if(pricesAfterBuying.length === 0) { return 0 } // 1
+  let maxPriceAfterBuying = Math.max(...pricesAfterBuying) // O(n)
+  if(maxPriceAfterBuying === minPrice) {   // 1
+    return 0
+  } else {
+    return (maxPriceAfterBuying - minPrice)
+  }
 }
+
+console.log(buyAndSellStocks([7,1,5,3,6,4]));
+console.log(buyAndSellStocks([7,6,4,3,1]));
   
-console.log(twoSum([2,7,11,15], 9)) // [0, 1]
-console.log(twoSum([3,2,4], 6)) // [1,2]
-console.log(twoSum([3,3], 6)) // [0,1]
-  
-// Time Complexity - O(n²);
+
+// Time Complexity = 3 x O(n) + 2 = O(n)
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
