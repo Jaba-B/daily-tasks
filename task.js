9d95f67739e6848fdf2bcdb24fa6fca139ea2e7b
Jaba Beroshvili <jb.beroshvili@gmail.com>
2022-10-14 11:39:17 +0400

    Duplicate Zeros

--- task.js
+++ task.js
@@ -1,27 +1,24 @@
-// Best Time to Buy and Sell Stock
+// Duplicate Zeros
 
-function buyAndSellStocks(arr) {
-  let minPrice = Math.min(...arr)  // O(n)
-  let pricesAfterBuying = arr.slice(arr.indexOf(minPrice) + 1) // O(n)
-  if(pricesAfterBuying.length === 0) { return 0 } // 1
-  let maxPriceAfterBuying = Math.max(...pricesAfterBuying) // O(n)
-  if(maxPriceAfterBuying === minPrice) {   // 1
-    return 0
-  } else {
-    return (maxPriceAfterBuying - minPrice)
+let arr = [1,0,2,3,0,4,5,0]
+
+function duplicateZeros(arr) {
+  let addedZeros = 0;  // 1
+  let copyedArray = [...arr];  // 1
+  for (let i = 0; i < copyedArray.length; i++) {  // O(n)
+    if (copyedArray[i] === 0 ) {
+      arr.splice(i + addedZeros, 0, 0);
+      arr.pop();
+      addedZeros += 1;
+    }
   }
 }
 
-console.log(buyAndSellStocks([7,1,5,3,6,4]));
-console.log(buyAndSellStocks([7,6,4,3,1]));
-  
+duplicateZeros(arr);
+console.log(arr)
 
-// Time Complexity = 3 x O(n) + 2 = O(n)
+// Time complexity = O(n) + 2 = O(n)
 
-// Input: prices = [7,1,5,3,6,4]
-// Output: 5
-// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+//Example 1:  Input: arr = [1,0,2,3,0,4,5,0];  Output: [1,0,0,2,3,0,0,4];  
 
-// Input: prices = [7,6,4,3,1]
-// Output: 0
-// Explanation: In this case, no transactions are done and the max profit = 0.
+//Example 2: Input: arr = [1,2,3];  Output: [1,2,3];
